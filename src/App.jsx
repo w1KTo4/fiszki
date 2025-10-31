f2efc5d0f8b6bb9e2ca33edc39679dfd29577e18
w1KTo4 <wiktor.rutkowski@tm1.edu.pl>
2025-10-31 12:01:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import "App.css";
+import "./App.css";
 
 export default function App() {
   const [ileInput, setIleInput] = useState("1-10");
